d6e225a1272050cf7ac41bddcef907778c922471
Ra'ees <66970886+gafoorraees@users.noreply.github.com>
2023-05-06 21:20:56 -0400

    Update 1.triangles.js

--- easy/1.triangles.js
+++ easy/1.triangles.js
@@ -5,7 +5,7 @@ class Triangle {
     this.side3 = side3;
     
     if (this.side1 === 0 || this.side2 === 0 || this.side3 === 0) {
-      throw new error()
+      throw new error();
     }
 
     if ((this.side1 + this.side2 <= this.side3) || (this.side1 + this.side3 <= this.side2) || (this.side2 + this.side3 <= this.side1)) {
