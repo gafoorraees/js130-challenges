1c8b6480f553b9b109cc0365c65c1e30ef06f157
Ra'ees <66970886+gafoorraees@users.noreply.github.com>
2023-05-08 12:44:18 -0400

    Update 7.octal.js

--- easy/7.octal.js
+++ easy/7.octal.js
@@ -1,30 +1,3 @@
-/*
-input: string
-output: number
-
-rules:
-  -octal numbers use a base of 8
-  if inoput is invalid, treat it as octal 0
-
-
-algorithm:
-  declare a class Octal
-  -define a constructor that takes a string as an argument
-  -declare a toDecimal method
-  -define a sum variable initialized to 0
-  -multiply the current number by 8 raised to the power of its index.
-  -reverse the input string
-  -declare a for loop. set the index to 0, condition to length of the reversed string
-    -convert the current element to a number, multiply it by 8, raise it to power of current index
-      -add it to the sum
-  return sum
-
-  -declare a validty method
-    -if input includes 8 or 9, it is invalid, return  0;
-    -if input includes letters, it is invalid return 0;
-    -
-*/
-
 class Octal {
   constructor(input) {
     this.input = input;
