4fc1fac866103e28262761e3cd92036cb64d34f6
Ra'ees Gafoor <gafoorraees@gmail.com>
2023-05-12 18:44:13 -0400

    Given a string of digitts, return all possible consecutive nubmer series of a specified length

---
+++ easy/10.series.js
@@ -0,0 +1,24 @@
+class Series {
+  constructor(digits) {
+    this.digits = digits;
+  }
+
+  slices(length) {
+    if (length > this.digits.length) {
+      throw new Error();
+    }
+
+    let digits = this.digits.split('').map(digit => Number(digit));
+    let series = [];
+    
+    for (let idx = 0; idx <= this.digits.length - length; idx += 1) {
+      let arr = digits.slice(idx, idx + length);
+      series.push(arr);
+    
+    }
+
+    return series;
+  }
+}
+
+module.exports = Series;
