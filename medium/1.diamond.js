2067dbfeba7ffae8701aac63341a8b900d05963c
Ra'ees Gafoor <gafoorraees@gmail.com>
2023-05-14 12:32:54 -0400

    Given a letter, this program prints a diamond starting with "A" and has at its widest point the supplied letter

---
+++ medium/1.diamond.js
@@ -0,0 +1,28 @@
+class Diamond {
+  static makeDiamond(letter) {
+    let startingLetter = 'A'.charCodeAt();
+    let endingLetter = letter.toUpperCase().charCodeAt();
+    let distance = endingLetter - startingLetter
+    let space = 1;
+    let topHalf = '';
+    let bottomHalf = '';
+    
+    for (let idx = distance - 1; idx >= 0; idx -= 1) {
+      topHalf += ' '.repeat(idx) + String.fromCharCode(endingLetter - idx) + ' '.repeat(space) + String.fromCharCode(endingLetter - idx) + ' '.repeat(idx) + '\n';
+      space += 2;
+    }
+
+    space = space - 2
+    
+    for (let idx = 1; idx <= distance - 1; idx += 1) {
+      space -= 2;
+      bottomHalf += ' '.repeat(idx) + String.fromCharCode(endingLetter - idx) + ' '.repeat(space) + String.fromCharCode(endingLetter - idx) + ' '.repeat(idx) + '\n';
+    }
+
+    let diamond = ' '.repeat(distance) + 'A' + ' '.repeat(distance) + '\n' + topHalf  + bottomHalf + ' '.repeat(distance) + 'A' + ' '.repeat(distance) + '\n';
+
+    return letter === 'A' ? 'A\n' : diamond;
+  }
+}
+
+module.exports = Diamond;
